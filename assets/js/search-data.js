6afc439706fa54833942e5f57fccbb9cc478027a
parth-potdar <98214550+parth-potdar@users.noreply.github.com>
2026-04-15 10:07:28 +0000

    Deploying to gh-pages from @ parth-potdar/parth-potdar.github.io@dff7934acd2ab1f8059a5096cd2bc7aa4a265265 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,13 +23,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/projects/";
           },
-        },{id: "nav-blog",
-          title: "Blog",
-          description: "",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/blog/";
-          },
         },{id: "books-the-godfather",
           title: 'The Godfather',
           description: "",
